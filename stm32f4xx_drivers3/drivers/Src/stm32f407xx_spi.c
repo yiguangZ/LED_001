cc5cf6e63ffeb3aa2f526c055d512ab3778d8220
yig88 <yig88@DESKTOP-43HMOKB.lan>
2025-08-03 15:39:48 -0500

    receive done

--- stm32f4xx_drivers3/drivers/Src/stm32f407xx_spi.c
+++ stm32f4xx_drivers3/drivers/Src/stm32f407xx_spi.c
@@ -141,7 +141,22 @@ void SPI_SendData(SPI_RegDef_t *pSPIx, uint8_t *pTxBuffer, uint32_t Len){
 	* @param  Len : length of data
 	* @return None
 	*/
-void SPI_ReceiveData(SPI_RegDef_t *pSPIx, uint8_t *pRxBuffer, uint32_t Len);
+void SPI_ReceiveData(SPI_RegDef_t *pSPIx, uint8_t *pRxBuffer, uint32_t Len){
+	while(Len>0){
+		while(SPI_GetFlagStatus(pSPIx, SPI_RXNE_FLAG) == RESET);//blocking and polling
+		if(pSPIx->CR1 & (1<<SPI_CR1_DFF)){
+			*((uint16_t*)pRxBuffer) = pSPIx->DR;
+			Len--;
+			Len--;
+			(uint16_t*)pRxBuffer++;
+		}
+		else {
+			*(pRxBuffer) = pSPIx->DR;
+			Len--;
+			pRxBuffer++;
+		}
+	}
+}
 /**
  * @brief   enable/disable an IRQ in the NVIC for a given SPI line.
  * @param  IRQNum  : IRQ number corresponding to EXTI line
