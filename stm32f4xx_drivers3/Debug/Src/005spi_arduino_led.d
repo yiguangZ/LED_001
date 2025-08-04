67fcaa3698f22e679fc42d8e1a72c11a4036fada
yig88 <yig88@DESKTOP-43HMOKB.lan>
2025-08-04 17:30:04 -0500

    s

---
+++ stm32f4xx_drivers3/Debug/Src/005spi_arduino_led.d
@@ -0,0 +1,9 @@
+Src/005spi_arduino_led.o: ../Src/005spi_arduino_led.c \
+ C:/Users/yig88/git/LED_001/stm32f4xx_drivers3/drivers/Inc/stm32f407xx.h \
+ C:/Users/yig88/git/LED_001/stm32f4xx_drivers3/drivers/Inc/stm32f407xx_gpio.h \
+ C:/Users/yig88/git/LED_001/stm32f4xx_drivers3/drivers/Inc/stm32f407xx.h \
+ C:/Users/yig88/git/LED_001/stm32f4xx_drivers3/drivers/Inc/stm32f407xx_spi_driver.h
+C:/Users/yig88/git/LED_001/stm32f4xx_drivers3/drivers/Inc/stm32f407xx.h:
+C:/Users/yig88/git/LED_001/stm32f4xx_drivers3/drivers/Inc/stm32f407xx_gpio.h:
+C:/Users/yig88/git/LED_001/stm32f4xx_drivers3/drivers/Inc/stm32f407xx.h:
+C:/Users/yig88/git/LED_001/stm32f4xx_drivers3/drivers/Inc/stm32f407xx_spi_driver.h:
