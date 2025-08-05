e589149da20a52f6271aa6a04f9df99aa02d7f7c
yig88 <yig88@DESKTOP-43HMOKB.lan>
2025-08-05 15:23:02 -0500

    redo

--- stm32f4xx_drivers3/Src/005spi_arduino_led.c
+++ stm32f4xx_drivers3/Src/005spi_arduino_led.c
@@ -35,7 +35,7 @@ void SPI2_GPIOInit(void){
 	SPIPins.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_ALTFN;
 	SPIPins.GPIO_PinConfig.GPIO_PinAltFunMode = 5;
 	SPIPins.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
-	SPIPins.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+	SPIPins.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PU;
 	SPIPins.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
 	//SCLK
 	SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_No_13;
@@ -48,7 +48,6 @@ void SPI2_GPIOInit(void){
 	GPIO_Init(&SPIPins);
 	//NSS
 	SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_No_12;
-	SPIPins.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PU;
 	GPIO_Init(&SPIPins);
 
 }
